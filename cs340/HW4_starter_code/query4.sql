c6213a9a8cf1266f5126724b57c77f0a76325573
Nathanael Butler <njbutler99@gmail.com>
2020-10-22 18:43:55 -0700

    initializing repo

---
+++ cs340/HW4_starter_code/query4.sql
@@ -0,0 +1,3 @@
+-- Find the actor_id, first_name and last_name of all actors who have never been in a Sci-Fi film.
+-- Order by the actor_id in ascending order.
+-- Put your query for Q4 here
---
+++ cs340/HW4_starter_code/query5.sql
@@ -0,0 +1,3 @@
+-- Find the film_title of all films which feature both KIRSTEN PALTROW and WARREN NOLTE
+-- Order the results by film_title in descending order.
+
